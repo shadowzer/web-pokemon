f257772952c4fefa9770e9297aef7c402193af9e
shad <shadow.mr96@gmail.com>
2016-10-03 09:44:51 +0500

    1. Refactored sprintf

--- Menu.php
+++ Menu.php
@@ -8,9 +8,9 @@ class Menu
         $ans = '<div class="links"><span class="space"></span>';
         foreach (self::$items as $key => $value) {
             if ($pageName == $key)
-                $ans .= sprintf("%s%s%s%s%s", '<span class="span"> <a class="linkChosed" href=index.php?page=', $pageName, '>', self::$items[$key], '</a> </span>');
+                $ans .= sprintf("<span class='span'> <a class='linkChosed' href=index.php?page=%s>%s</a> </span>", $pageName, $value);
             else
-                $ans .= sprintf("%s%s%s%s%s", '<span class="span"> <a class="link" href=index.php?page=', $key, '>', self::$items[$key], '</a> </span>');
+                $ans .= sprintf("<span class='span'> <a class='link' href=index.php?page=%s>%s</a> </span>", $key, $value);
         }
         $ans .= '</div>';
         return $ans;
